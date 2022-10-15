62ed893b8d7d545eceee00bdaee21d5dd092414f
Mato <Kittawat@icloud.com>
2022-10-16 01:14:57 +0800

    Initial Commit

---
+++ TwitterSwiftUiYoutube/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUiYoutube
+//
+//  Created by kittawat phuangsombat on 2022/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterSwiftUiYoutube/TwitterSwiftUiYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUiYoutubeApp.swift
+//  TwitterSwiftUiYoutube
+//
+//  Created by kittawat phuangsombat on 2022/10/16.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUiYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
