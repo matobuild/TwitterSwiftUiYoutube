21916975896ba5c201cb2beb4062aad8d3e0e628
Mato <Kittawat@icloud.com>
2022-10-23 21:51:20 +0800

    created the side menu

---
+++ TwitterSwiftUiYoutube/Core/SideMenu/VIews/SideMenuView.swift
@@ -0,0 +1,64 @@
+//
+//  SideMenuView.swift
+//  TwitterSwiftUiYoutube
+//
+//  Created by kittawat phuangsombat on 2022/10/22.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        
+        VStack(alignment: .leading, spacing: 32) {
+            VStack(alignment: .leading) {
+                Circle()
+                    .frame(width: 48, height: 48)
+                
+                VStack(alignment: .leading,spacing: 4) {
+                    Text("Bruce Wayne")
+                        .font(.headline)
+                    
+                    Text("@Batman")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                    
+                }
+                
+                UserStatsView()
+                    .padding(.vertical)
+            }
+            .padding(.leading)
+            
+            ForEach(SideMenuViewModel.allCases, id: \.rawValue) { viewModel in
+                if viewModel == .profile {
+                    NavigationLink {
+                        ProfileView()
+                    } label: {
+                        SideMenuOptionRowView(viewModel: viewModel)
+                    }
+                } else if viewModel == .logout {
+                    Button {
+                        print("handle logout here")
+                    } label: {
+                        SideMenuOptionRowView(viewModel: viewModel)
+                    }
+
+                } else {
+                    SideMenuOptionRowView(viewModel: viewModel)
+                }
+                
+              
+            }
+            
+            Spacer()
+            
+        }
+    }
+}
+
+struct SideMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuView()
+    }
+}
